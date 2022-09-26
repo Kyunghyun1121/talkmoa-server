9b02fc489a4e3975bf85b092106930db9d3a673a
Seong Heung Bae <mopil1102@gmail.com>
2022-09-26 20:43:07 +0900

    Add files via upload

---
+++ src/main/java/com/talkmoaserver/TalkmoaServerApplication.java
@@ -0,0 +1,13 @@
+package com.talkmoaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkmoaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkmoaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talkmoaserver/TalkmoaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkmoaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkmoaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
