7f8a4876f1d69879c150a7a1c693c4d48ab4ecc3
Kyunghyun1121 <90241597+Kyunghyun1121@users.noreply.github.com>
2022-09-30 22:18:57 +0900

    서비스 만들어봤습니다.

--- src/main/java/com/talkmoaserver/entity/ChatRoom.java
+++ src/main/java/com/talkmoaserver/entity/ChatRoom.java
@@ -13,6 +13,45 @@ public class ChatRoom {
     private String roomName;
     private int totalWordCount;
 
+
+
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Word> wordList = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public int getTotalWordCount() {
+        return totalWordCount;
+    }
+
+    public void setTotalWordCount(int totalWordCount) {
+        this.totalWordCount = totalWordCount;
+    }
+
+    public List<Word> getWordList() {
+        return wordList;
+    }
+
+    public void setWordList(List<Word> wordList) {
+        this.wordList = wordList;
+    }
+
+
+
+
+
 }
